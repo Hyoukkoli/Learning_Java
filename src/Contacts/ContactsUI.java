b2b15ed8091db243cfc08093b8d39c98fe9be126
Hyoukkoli <hyoukai@hotmail.de>
2017-05-09 18:08:42 +0200

    added folder Contacts and its content

---
+++ src/Contacts/ContactsUI.java
@@ -0,0 +1,11 @@
+package Contacts;
+
+import javax.swing.*;
+
+/**
+ * Created by Andrea on 09.05.2017.
+ */
+public class ContactsUI {
+    private JTextField test1;
+    private JButton button1;
+}
